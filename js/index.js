880b1d0db474a902b40d073646180ee5c43e7ad7
Cody Milliron <codymilliron.dev@gmail.com>
2023-07-09 23:26:04 -0400

    Name all images. Build first element of the fiber pages.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,1 @@
 
-
-const setCopyrightDate = () => {
-    const date = new Date().getFullYear();
-    const dateLocation = document.getElementById("date");
-    
-    dateLocation.innerHTML = date;
-
-
-}
-
-setCopyrightDate()
